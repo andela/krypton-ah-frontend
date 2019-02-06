9f6d5e954fb947b85fb8a5ac2fe5744b22af996d
ibrahimalausa <ibrahim.alausa@andela.com>
2019-02-06 08:09:47 +0100

    [ch-#162414110 create initial project setup]
    - Setup an empty react project
    - Setup react router version 4 (React-router-dom)
    - Setup Redux
    - Setup SASS processing on the client
    - Setup Semantic UI React
    - Add normalize CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
 import 'normalize.css';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/index.scss';
 import App from './App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
