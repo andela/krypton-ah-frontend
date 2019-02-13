bff7edf6c8f6d33337615e63ca72d0486b85c6a6
kabiribraheem <ibraheemkabir9@gmail.com git config --global user.name kaniribraheem git config --global user.email ibraheemkabir9@gmail.com>
2019-02-13 01:15:41 +0100

    implement landing page react componenents

---
+++ src/components/widgets/articleswidgets/reactionWidgets.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Menu, Icon, Rating } from 'semantic-ui-react';
+import './reactionwidgets.scss';
+
+export default function articleswidgets(props) {
+  const {
+    date, ratingIcon, numberoflikes, numberofdislikes
+  } = props;
+  return (
+    <div>
+      <Menu className="widgetContainer">
+        <div className="Icons">
+          <i className="dates">{date}</i>
+          <Icon link disabled size="small" fitted name="thumbs up outline" />
+          {numberoflikes}
+          <Icon link disabled size="small" fitted name="thumbs down outline" />
+          {numberofdislikes}
+          <Icon link size="small" name="bookmark outline" />
+          <Rating
+            maxRating={5}
+            defaultRating={3}
+            icon="star"
+            size="small"
+            className={ratingIcon}
+          />
+        </div>
+      </Menu>
+    </div>
+  );
+}
+
+articleswidgets.defaultProps = {
+  ratingIcon: 'rating_class',
+  date: '28th february 2019',
+  numberoflikes: '23',
+  numberofdislikes: '23'
+};
+
+articleswidgets.propTypes = {
+  ratingIcon: PropTypes.string,
+  date: PropTypes.string,
+  numberoflikes: PropTypes.number,
+  numberofdislikes: PropTypes.number
+};
