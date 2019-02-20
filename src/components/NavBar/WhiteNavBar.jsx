e949191765790b6a3429c6005ff272239e3ad93d
Afolayan Isaiah Kunle <iafolayanibikunle@gmail.com>
2019-02-20 09:33:06 +0100

    Update WhiteNavrBar
    Added a link to login

--- src/components/NavBar/WhiteNavBar.jsx
+++ src/components/NavBar/WhiteNavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, Image, Menu, Icon, Button } from 'semantic-ui-react';
+import { Dropdown, Image, Menu, Icon, Button, Link } from 'semantic-ui-react';
 import AHIcon from '../../images/Logo.png';
 import avatarPlaceholder from '../../images/avatar.png';
 
@@ -20,7 +20,9 @@ export default function WhiteNavBar({ fixed, isAuthenticated, avatarUrl }) {
           <Icon fitted size="big" name="search" />
         </Menu.Item>
         <Menu.Item>
-          <Button secondary>Write an Article</Button>
+          <Link to="/login">
+            <Button secondary>Write an Article</Button>
+          </Link>
         </Menu.Item>
         {isAuthenticated ? (
           <Menu.Item>
