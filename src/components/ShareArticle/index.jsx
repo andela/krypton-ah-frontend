7de1ae6b3dabf7e4fcf4b81a8ed4d152fdd91601
Isaiah Afolayan <isaiahafolayan@Isaiahs-MacBook-Pro.local>
2019-02-26 20:05:58 +0100

    [ft-share-article] fix responsiveness on share article

--- src/components/ShareArticle/index.jsx
+++ src/components/ShareArticle/index.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   FacebookShareButton,
-  GooglePlusShareButton,
   LinkedinShareButton,
   TwitterShareButton,
   WhatsappShareButton,
   EmailShareButton,
   FacebookIcon,
   TwitterIcon,
-  GooglePlusIcon,
   LinkedinIcon,
   WhatsappIcon,
   EmailIcon } from 'react-share';
@@ -26,14 +24,12 @@ class ShareArticle extends React.Component {
     const { id, title, description } = this.props;
     const url = `${BASE_URL_CB}/article/${id}`;
     const hashtags = ['ah-Krypton'];
+    const emailURL = `${description}(<br />) ${url}`;
     return (
       <div className="shareArticleIcons">
         <FacebookShareButton quote={description} url={url}>
           <FacebookIcon size={25} round />
         </FacebookShareButton>
-        <GooglePlusShareButton quote={description} url={url}>
-          <GooglePlusIcon size={25} round />
-        </GooglePlusShareButton>
         <TwitterShareButton title={title} hashtags={hashtags} url={url}>
           <TwitterIcon size={25} round />
         </TwitterShareButton>
@@ -43,7 +39,7 @@ class ShareArticle extends React.Component {
         <WhatsappShareButton title={title} url={url}>
           <WhatsappIcon size={25} round />
         </WhatsappShareButton>
-        <EmailShareButton subject={title} body={description} url={url}>
+        <EmailShareButton subject={title} body={emailURL} url={url}>
           <EmailIcon size={25} round />
         </EmailShareButton>
 
