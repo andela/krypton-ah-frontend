4bcd5a62d244085da06a50930d8e3aa03500a5d4
ibrahimalausa <ibrahim.alausa@andela.com>
2019-02-26 12:05:32 +0100

    save work before demo branch

--- src/components/widgets/ReactionIcons.jsx
+++ src/components/widgets/ReactionIcons.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-export default function ReactionIcons({
-  date, numberoflikes, numberofdislikes, numberofcomments
-}) {
-  return (
-    <div>
-      <React.Fragment>
-        <i className="dates">{date}</i>
-        <Icon disabled link size="small" fitted name="thumbs up outline" />
-        {numberoflikes}
-        <Icon disabled link size="small" fitted name="thumbs down outline" />
-        {numberofdislikes}
-        <Icon disabled link size="small" fitted name="comments outline" />
-        {numberofcomments}
-      </React.Fragment>
-    </div>
-  );
+export default class ReactionIcons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likeActive: true,
+      dislikeActive: true
+    };
+  }
+
+  likeArticle = (outline = 'outline') => {
+    const name = outline.length === 0 ? 'thumbs up' : `thumbs up ${outline}`;
+    return <Icon disabled link size="small" fitted name={name} />;
+  };
+
+  dislikeArticle = (outline = 'outline') => {
+    const name = outline.length === 0 ? 'thumbs down' : `thumbs down ${outline}`;
+    return <Icon disabled link size="small" fitted name={`thumbs down ${name}`} />;
+  };
+
+  render() {
+    const { date, numberoflikes, numberofdislikes, numberofcomments } = this.props;
+    const { likeActive, dislikeActive } = this.state;
+    const outline = '';
+    return (
+      <div>
+        <React.Fragment>
+          <i className="dates">{date}</i>
+          {likeActive ? this.likeArticle(outline) : this.likeArticle()}
+          {numberoflikes}
+          {dislikeActive ? this.dislikeArticle(outline) : this.dislikeArticle()}
+          {numberofdislikes}
+          <Icon disabled link size="small" fitted name="comments outline" />
+          {numberofcomments}
+        </React.Fragment>
+      </div>
+    );
+  }
 }
 ReactionIcons.defaultProps = {
   date: '28th february 2019',
