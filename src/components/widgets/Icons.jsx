642fd320726aeff7eabdbeb53a9f22ebbb970ac2
kabiribraheem <ibraheemkabir9@gmail.com git config --global user.name kaniribraheem git config --global user.email ibraheemkabir9@gmail.com>
2019-03-01 09:57:01 +0100

    setup-reducer-and-action

--- src/components/widgets/Icons.jsx
+++ src/components/widgets/Icons.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import { Menu, Icon, Rating } from 'semantic-ui-react';
 import Reactions from './ReactionIcons';
 
+<<<<<<< HEAD
 export default function Icons({ id, ratingIcon, numberofcomments, date, name }) {
+=======
+export default function Icons(props) {
+  const { ratingIcon, numberofcomments, date, name, rating, disabled } = props;
+>>>>>>> setup-reducer-and-action
   return (
     <Menu className="widgetContainer">
       <div className="icons">
@@ -13,6 +18,7 @@ export default function Icons({ id, ratingIcon, numberofcomments, date, name }) 
           </React.Fragment>
         ) : (
           <React.Fragment>
+<<<<<<< HEAD
               <Reactions date={date} selectedArticleId={id} />
               <Icon disabled link size="small" name="bookmark outline" />
               <span>{numberofcomments}</span>
@@ -21,6 +27,16 @@ export default function Icons({ id, ratingIcon, numberofcomments, date, name }) 
                 <span>{name}</span>
               </div>
             </React.Fragment>
+=======
+            <Reactions date={date} />
+            <Icon disabled link size="small" name="bookmark outline" />
+            <span>{numberofcomments}</span>
+            <div className="rating">
+              <Rating disabled={disabled} maxRating={5} defaultRating={rating} size="small" />
+              {name ? <span>{name}</span> : ''}
+            </div>
+          </React.Fragment>
+>>>>>>> setup-reducer-and-action
         )}
       </div>
     </Menu>
--- src/containers/Homecontainer/featuredArticles.jsx
+++ src/containers/Homecontainer/featuredArticles.jsx
@@ -31,9 +31,14 @@ export default function FeaturedArticles({ articles }) {
                   articles.data[0].articleAuthor.lastname
                 }`}
 <<<<<<< HEAD
+<<<<<<< HEAD
                 id={article.data[0].id}
 =======
 >>>>>>> landing-page-logic
+=======
+                rating={article.data[0].averageRating}
+                disabled
+>>>>>>> setup-reducer-and-action
               />
             </Card>
           </Grid.Column>
