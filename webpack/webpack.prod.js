5d15401dda4fd90d255dcfd8d93044fbaa545a81
Adeniran Mark <32283153+Maxfurry@users.noreply.github.com>
2019-03-05 22:15:10 +0100

    Update webpack.prod.js
    add cloudinary details

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -16,7 +16,10 @@ module.exports = merge(common, {
         LINKEDIN_URL: JSON.stringify(process.env.LINKEDIN_URL),
         TWITTER_URL: JSON.stringify(process.env.TWITTER_URL),
         API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
-        BASE_URL_CB: JSON.stringify(process.env.BASE_URL_CB)
+        BASE_URL_CB: JSON.stringify(process.env.BASE_URL_CB),
+        CLOUDINARY_UPLOAD_PRESET: JSON.stringify(process.env.CLOUDINARY_UPLOAD_PRESET),
+        CLOUDINARY_API_KEY: JSON.stringify(process.env.CLOUDINARY_API_KEY),
+        CLOUDINARY_USERNAME: JSON.stringify(process.env.CLOUDINARY_USERNAME)
       }
     })
   ]
