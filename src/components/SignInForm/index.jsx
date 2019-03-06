cb5fd630b9f1b36624a5ab8b47943ae1c1a94e1b
Isaiah Afolayan <isaiahafolayan@Isaiahs-MacBook-Pro.local>
2019-03-06 10:33:17 +0100

    fix styling

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -51,7 +51,7 @@ class SignIn extends React.Component {
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Group widths="equal">
-          <Form.Input type="email" className="email" fluid placeholder="Email Address" name="email" value={user.email} onChange={this.handleChange} required />
+          <Form.Input type="email" className="email" id="emailAddress" fluid placeholder="Email Address" name="email" value={user.email} onChange={this.handleChange} required />
         </Form.Group>
         {errors.email && <InlineError text={errors.email} />}
         <Form.Group widths="equal" fluid>
